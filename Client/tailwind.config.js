3583843934bd8753a60f8075e9f23a42d1aac549
David John <davidajohn@gmail.com>
2022-07-05 17:56:13 +0100

    Added tailwind to Client project and created basic layout

---
+++ Client/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/*.razor",
+        "./Pages/**/*.razor",
+        "./Shared/*.razor",
+        "./Components/*.razor",
+        "./App.razor"
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem'
+        },
+        extend: {
+            spacing: {
+                '112': '28rem',
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+            fontFamily: {
+                body: ['Roboto']
+            }
+        },
+  },
+  plugins: [],
+}
