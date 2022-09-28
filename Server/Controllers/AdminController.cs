493b528255b4d49d170a29bf8d348f5e31972758
David John <davidajohn@gmail.com>
2022-09-28 23:52:57 +0100

    Created Admin controller and refactored existing gallery controller methods

---
+++ Server/Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PhotoPortfolio.Shared.Entities;
+
+namespace PhotoPortfolio.Server.Controllers;
+
+//[Authorize]
+public class AdminController : BaseApiController
+{
+    private readonly IGalleryRepository _repository;
+
+    public AdminController(IGalleryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("galleries")]
+    public async Task<List<Gallery>> GetAllGalleries()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    [HttpGet("galleries/{id}")]
+    public async Task<IActionResult> GetGalleryById(string id)
+    {
+        var gallery = await _repository.GetGalleryWithPhotos(id);
+
+        if (gallery == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(gallery);
+    }
+
+    [HttpPost("galleries")]
+    public async Task<IActionResult> AddGallery(Gallery gallery)
+    {
+        await _repository.AddAsync(gallery);
+
+        return CreatedAtAction(nameof(GetAllGalleries), new { id = gallery.Id }, gallery);
+    }
+
+    [HttpPut("galleries/{id:length(24)}")]
+    public async Task<IActionResult> UpdateGallery(string id, Gallery gallery)
+    {
+        var galleryToUpdate = await _repository.GetSingleAsync(x => x.Id == id);
+
+        if (galleryToUpdate is null)
+        {
+            return NotFound();
+        }
+
+        gallery.Id = galleryToUpdate.Id;
+
+        await _repository.UpdateAsync(gallery);
+
+        return NoContent();
+    }
+
+    [HttpDelete("galleries/{id:length(24)}")]
+    public async Task<IActionResult> DeleteGallery(string id)
+    {
+        var galleryToDelete = await _repository.GetSingleAsync(x => x.Id == id);
+
+        if (galleryToDelete is null)
+        {
+            return NotFound();
+        }
+
+        await _repository.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
--- Server/Controllers/GalleriesController.cs
+++ Server/Controllers/GalleriesController.cs
@@ -13,7 +13,7 @@ public class GalleriesController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<List<Gallery>> GetGalleries()
+    public async Task<List<Gallery>> GetPublicGalleries()
     {
         return await _repository.GetAllAsync();
     }
@@ -29,45 +29,5 @@ public class GalleriesController : BaseApiController
         }
 
         return Ok(gallery);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> AddGallery(Gallery gallery)
-    {
-        await _repository.AddAsync(gallery);
-
-        return CreatedAtAction(nameof(GetGalleries), new { id = gallery.Id }, gallery);
-    }
-
-    [HttpPut("{id:length(24)}")]
-    public async Task<IActionResult> UpdateGallery(string id, Gallery gallery)
-    {
-        var galleryToUpdate = await _repository.GetSingleAsync(x => x.Id == id);
-
-        if (galleryToUpdate is null)
-        {
-            return NotFound();
-        }
-
-        gallery.Id = galleryToUpdate.Id;
-
-        await _repository.UpdateAsync(gallery);
-
-        return NoContent();
-    }
-
-    [HttpDelete("{id:length(24)}")]
-    public async Task<IActionResult> DeleteGallery(string id)
-    {
-        var galleryToDelete = await _repository.GetSingleAsync(x => x.Id == id);
-
-        if (galleryToDelete is null)
-        {
-            return NotFound();
-        }
-
-        await _repository.DeleteAsync(id);
-
-        return NoContent();
     }
 }
