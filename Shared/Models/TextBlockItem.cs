af98b0422c8561294e6b6b616ac40683b486a289
David John <davidajohn@gmail.com>
2022-09-29 22:05:32 +0100

    Created a TextBlock component to display custom text

---
+++ Shared/Models/TextBlockItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public class TextBlockItem
+{
+    public string Id { get; set; } = string.Empty;
+    public string TextBlockId { get; set; } = string.Empty;
+    public int BlockSortOrder { get; set; }
+    public string TextContent { get; set; } = string.Empty;
+    public string TextColour { get; set; } = "#000000";
+    public string TextSize { get; set; } = "md";
+    public string TextWeight { get; set; } = "normal";
+    public string TextAlignment { get; set; } = "left";
+}
