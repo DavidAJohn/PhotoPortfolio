157d592c7ed7c800491876ca41c1ad03217d2345
David John <davidajohn@gmail.com>
2022-09-29 18:19:02 +0100

    HeroImage component now allows custom vertical alignment

--- Shared/Entities/Gallery.cs
+++ Shared/Entities/Gallery.cs
@@ -5,6 +5,7 @@ public class Gallery : BaseEntity
     public string Name { get; set; } = "New Gallery";
     public string Description { get; set; } = string.Empty;
     public string HeaderImage { get; set; } = null!;
+    public string HeaderImageAlign { get; set; } = "center";
     public string? ParentGallery { get; set; }
     public string SortBy { get; set; } = string.Empty;
     public string SortOrder { get; set; } = string.Empty;
