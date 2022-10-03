b115e41c6bbb98b20e3a6d4606849ed5a73936ad
David John <davidajohn@gmail.com>
2022-10-03 20:19:36 +0100

    Major update to Carousel, which now accepts a list of images to be displayed

---
+++ Shared/Models/CarouselItem.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public enum ImageAlignment
+{
+    Top,    // 0
+    Middle, // 1
+    Bottom, // 2
+}
+
+public class CarouselItem
+{
+    public string Uri { get; set; } = string.Empty;
+    public ImageAlignment ImageAlign { get; set; } = ImageAlignment.Middle;
+}
