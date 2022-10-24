4d85c18fc59afc66bc4d6f8b9898c0c206975dad
David John <davidajohn@gmail.com>
2022-10-24 21:13:28 +0100

    Added tests for the Dropdown component

--- Shared/Models/DropdownItem.cs
+++ Shared/Models/DropdownItem.cs
@@ -2,6 +2,7 @@
 
 public class DropdownItem
 {
+    public int Id { get; set; }
     public string OptionName { get; set; } = string.Empty;
     public string OptionRef { get; set; } = string.Empty;
 }
