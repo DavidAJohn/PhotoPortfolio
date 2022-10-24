1b79627a5ce032611e06e5f7cbc94238ffd41f73
David John <davidajohn@gmail.com>
2022-10-24 18:16:29 +0100

    Created new Dropdown component and added it to Photo Details page to show print options

---
+++ Shared/Models/DropdownItem.cs
@@ -0,0 +1,7 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public class DropdownItem
+{
+    public string OptionName { get; set; } = string.Empty;
+    public string OptionRef { get; set; } = string.Empty;
+}
