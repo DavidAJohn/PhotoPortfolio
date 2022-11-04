1f9e9dae0cc23f957c2c5c0730c2d39c9d432164
David John <davidajohn@gmail.com>
2022-11-04 18:20:38 +0000

    Added a snackbar message to the admin galleries page

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using PhotoPortfolio.Client;
 using PhotoPortfolio.Client.Contracts;
@@ -48,7 +49,17 @@ builder.Services.AddScoped<IAdminService, AdminService>();
 
 builder.Services.AddBlazoredModal();
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 await builder.Build().RunAsync();
 
