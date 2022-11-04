03fc2c1ca9c52719a13a419651dc3cc9d4a7b982
David John <davidajohn@gmail.com>
2022-11-04 15:29:04 +0000

    Added an admin service to the client project

---
+++ Client/Contracts/IAdminService.cs
@@ -0,0 +1,8 @@
+﻿using PhotoPortfolio.Shared.Entities;
+
+namespace PhotoPortfolio.Client.Contracts;
+
+public interface IAdminService
+{
+    Task<List<Gallery>> GetAllGalleriesAsync();
+}
---
+++ Client/Services/AdminService.cs
@@ -0,0 +1,32 @@
+﻿using PhotoPortfolio.Client.Contracts;
+using PhotoPortfolio.Shared.Entities;
+using System.Net.Http.Json;
+
+namespace PhotoPortfolio.Client.Services;
+
+public class AdminService : IAdminService
+{
+    private readonly IHttpClientFactory _httpClient;
+
+    public AdminService(IHttpClientFactory httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Gallery>> GetAllGalleriesAsync()
+    {
+        try
+        {
+            var client = _httpClient.CreateClient("PhotoPortfolio.ServerAPI.Secure");
+            var galleries = await client.GetFromJsonAsync<List<Gallery>>("galleries");
+
+            if (galleries is null) return null!;
+
+            return galleries;
+        }
+        catch (HttpRequestException ex)
+        {
+            throw new HttpRequestException(ex.Message, ex.InnerException, ex.StatusCode);
+        }
+    }
+}
