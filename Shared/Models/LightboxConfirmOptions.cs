8f827226ce9e5f2a675de25a69508287ffeeaed6
David John <davidajohn@gmail.com>
2022-11-08 14:31:50 +0000

    LightboxModal now offers additional config options

---
+++ Shared/Models/LightboxConfirmOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public enum LightboxConfirmAction
+{
+    Navigate, // 0
+    NavigateForceLoad, // 1
+    CloseOnly // 2
+}
+
+public class LightboxConfirmOptions
+{
+    public bool ShowButton { get; set; } = true;
+    public string ButtonText { get; set; } = "Confirm";
+    public LightboxConfirmAction ButtonAction { get; set; } = LightboxConfirmAction.CloseOnly;
+    public string NavigateUri { get; set; } = "#";
+}
