751380cd95b57c451e8e821a1433436528ad493b
David John <davidajohn@gmail.com>
2022-11-22 18:08:46 +0000

    Added Serilog to the server project with a basic console sink around the Program class

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,66 +3,86 @@ using Microsoft.Identity.Web;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .WriteTo.Console()
+            .CreateLogger();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+try
+{
+    Log.Information("Starting web application");
 
-builder.Services.Configure<JwtBearerOptions>(
-    JwtBearerDefaults.AuthenticationScheme, options =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+    builder.Services.Configure<JwtBearerOptions>(
+        JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+            options.TokenValidationParameters.NameClaimType = "name";
+        });
+
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+    builder.Services.AddOcelot(builder.Configuration)
+                    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+    builder.Services.ConfigureOcelotPlaceholders(builder.Configuration); // custom extension: ./Helpers/FileConfigurationExtensions
+
+    builder.Services.AddSingleton<MongoContext>();
+
+    builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
+    builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+    builder.Services.AddControllers()
+        .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+    builder.Services.AddRazorPages();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
     {
-        options.TokenValidationParameters.NameClaimType = "name";
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+    app.MapWhen(context => context.Request.Path.StartsWithSegments("/prodigi"), appBuilder =>
+    {
+        appBuilder.UseOcelot();
     });
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
-
-builder.Services.AddOcelot(builder.Configuration)
-                .AddCacheManager(settings => settings.WithDictionaryHandle());
-
-builder.Services.ConfigureOcelotPlaceholders(builder.Configuration); // custom extension: ./Helpers/FileConfigurationExtensions
-
-builder.Services.AddSingleton<MongoContext>();
-
-builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
-builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.MapWhen(context => context.Request.Path.StartsWithSegments("/prodigi"), appBuilder =>
+finally
 {
-    appBuilder.UseOcelot();
-});
-
-app.Run();
+    Log.CloseAndFlush();
+}
