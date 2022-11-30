8d2961ed8a09d2df4a0a998221371728f7e41a04
David John <davidajohn@gmail.com>
2022-11-30 19:03:31 +0000

    Updated LightboxModal to accept a list of confirm/action buttons to display

---
+++ Shared/Models/ModalConfirmResult.cs
@@ -0,0 +1,9 @@
+﻿using PhotoPortfolio.Shared.Entities;
+
+namespace PhotoPortfolio.Shared.Models;
+
+public class ModalConfirmResult
+{
+    public Photo Photo { get; set; } = null!;
+    public LightboxConfirmOptions Button { get; set; } = null!;
+}
