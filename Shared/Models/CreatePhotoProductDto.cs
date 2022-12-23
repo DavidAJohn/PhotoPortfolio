e6009ab8e13d6519b58d980a79baa277595d8aa2
David John <davidajohn@gmail.com>
2022-12-23 17:40:43 +0000

    Improved validation on the Edit Photo page when adding a new product

---
+++ Shared/Models/CreatePhotoProductDto.cs
@@ -0,0 +1,19 @@
+﻿using PhotoPortfolio.Shared.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoPortfolio.Shared.Models;
+
+public class CreatePhotoProductDto : Product
+{
+    [Required(ErrorMessage = "Custom Description can not be empty")]
+    public string? CustomDescription { get; set; }
+
+    [Required(ErrorMessage = "Further Details can not be empty")]
+    public string? FurtherDetails { get; set; }
+
+    public string? MockupImageUri { get; set; }
+
+    [Required(ErrorMessage = "Markup Percentage can not be empty")]
+    [Range(100, 1000, ErrorMessage = "Value must be between {1} and {2}.")]
+    public int MarkupPercentage { get; set; } = 100;
+}
