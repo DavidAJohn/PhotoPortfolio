f6f3ca6acb36ffa5784e6554a8665f209caf51b2
David John <davidajohn@gmail.com>
2023-03-17 18:22:29 +0000

    Added some data annotations to the Order entity class

--- Shared/Entities/Order.cs
+++ Shared/Entities/Order.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using PhotoPortfolio.Shared.Models.Prodigi.Orders;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoPortfolio.Shared.Entities;
 
@@ -9,12 +10,23 @@ public class Order : BaseEntity
 {
     public string Name { get; set; }
     public string EmailAddress { get; set; }
+
+    [Required]
     public BsonDateTime OrderCreated { get; set; }
+
     public BsonDateTime? PaymentCompleted { get; set; }
+
+    [Required]
     public List<BasketItem> Items { get; set; }
+
+    [Required] 
     public BsonDecimal128 TotalCost { get; set; }
+
     public Address Address { get; set; }
+
+    [Required] 
     public string ShippingMethod { get; set; }
+
     public OrderStatus Status { get; set; } = OrderStatus.NotReady;
 }
 
