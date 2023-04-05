df7b44787396cd84cade735eebfd3a7d242e7567
David John <davidajohn@gmail.com>
2023-04-05 15:49:54 +0100

    Edit Photo page now allows updating of product markup percentage

--- Shared/Models/PhotoProduct.cs
+++ Shared/Models/PhotoProduct.cs
@@ -4,6 +4,15 @@ namespace PhotoPortfolio.Shared.Models;
 
 public class PhotoProduct : Product
 {
+    public PhotoProduct()
+    {
+    }
+
+    public PhotoProduct(Product product)
+    {
+        Id = product.Id;
+    }
+
     public string CustomDescription { get; set; } = string.Empty;
     public string FurtherDetails { get; set; } = string.Empty;
     public string MockupImageUri { get; set; } = string.Empty;
--- Shared/Models/ProductBasketItemDto.cs
+++ Shared/Models/ProductBasketItemDto.cs
@@ -1,7 +1,17 @@
-﻿namespace PhotoPortfolio.Shared.Models;
+﻿using PhotoPortfolio.Shared.Entities;
+
+namespace PhotoPortfolio.Shared.Models;
 
 public class ProductBasketItemDto : PhotoProduct
 {
+    public ProductBasketItemDto()
+    {
+    }
+
+    public ProductBasketItemDto(Product product) : base(product)
+    {
+    }
+
     public string ImageUri { get; set; } = string.Empty;
     public string ImageTitle { get; set; } = string.Empty;
 }
