f5f386792c207a59dba904764e98f1d1c0456c99
David John <davidajohn@gmail.com>
2023-05-23 23:49:18 +0100

    Improved Admin section navigation with a new Breadcrumb component. Closes #28

---
+++ Shared/Models/BreadcrumbCrumb.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public class BreadcrumbCrumb
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Uri { get; set; }
+    public bool Enabled { get; set; }
+}
