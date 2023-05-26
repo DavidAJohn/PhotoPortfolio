ca0543d0f0c0764816573e79fc9e99f4d0f73cfc
David John <davidajohn@gmail.com>
2023-05-26 23:18:04 +0100

    Created a template for the photo credits page with associated class and footer update

---
+++ Shared/Models/PhotoCredit.cs
@@ -0,0 +1,12 @@
+﻿using PhotoPortfolio.Shared.Entities;
+
+namespace PhotoPortfolio.Shared.Models;
+
+public class PhotoCredit : BaseEntity
+{
+    public int Order { get; set; }
+    public string CreatorName { get; set; }
+    public string CreatorUnsplashProfileUri { get; set; }
+    public string UnsplashImageUri { get; set; }
+    public string PhotoPortfolioImageUri { get; set; }
+}
