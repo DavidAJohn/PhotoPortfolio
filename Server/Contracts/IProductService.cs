3b0c26f0e871e54c6c0e6c0ae29b743db004b655
David John <davidajohn@gmail.com>
2023-06-06 23:40:50 +0100

    Added a product service to the Server project

---
+++ Server/Contracts/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using PhotoPortfolio.Shared.Models.Prodigi.Products;
+
+namespace PhotoPortfolio.Server.Contracts;
+
+public interface IProductService
+{
+    Task<ProductDetails> GetProductDetails(string sku);
+}
---
+++ Shared/Models/Prodigi/Products/ProductDetailsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace PhotoPortfolio.Shared.Models.Prodigi.Products;
+
+public class ProductDetailsResponse : BaseResponse
+{
+    public ProductDetails Product { get; set; } = null!;
+}
