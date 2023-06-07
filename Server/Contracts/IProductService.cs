0261039a3edad08f7cd97e504f180804f3cdd0c6
David John <davidajohn@gmail.com>
2023-06-07 23:15:34 +0100

    Removed the redundant product service class in the Server project

--- Server/Contracts/IProductService.cs
+++
@@ -1,8 +0,0 @@
-﻿using PhotoPortfolio.Shared.Models.Prodigi.Products;
-
-namespace PhotoPortfolio.Server.Contracts;
-
-public interface IProductService
-{
-    Task<ProductDetails> GetProductDetails(string sku);
-}
