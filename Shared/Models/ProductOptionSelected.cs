a7703a4a0f24c0aad446ead7de68c4fa5ddc87e5
David John <davidajohn@gmail.com>
2023-06-15 23:31:11 +0100

    Created a separate ProductOptionDropdown component to return more info on the selected option

---
+++ Shared/Models/ProductOptionSelected.cs
@@ -0,0 +1,8 @@
+﻿namespace PhotoPortfolio.Shared.Models;
+
+public class ProductOptionSelected
+{
+    public string OptionLabel { get; set; }
+    public string OptionRef { get; set; }
+    public string OptionName { get; set; }
+}
