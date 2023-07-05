2feff6b8c3407935a9bb042409a53538ae850aa3
David John <davidajohn@gmail.com>
2023-07-05 16:51:41 +0100

    Added tests for the Galleries and Photos controllers

---
+++ Tests/Server/Controllers/GalleriesControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using PhotoPortfolio.Server.Contracts;
+using PhotoPortfolio.Server.Controllers;
+using PhotoPortfolio.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoPortfolio.Tests.Server.Controllers;
+
+public class GalleriesControllerTests : BaseApiController
+{
+    private readonly GalleriesController _sut;
+    private readonly IGalleryRepository _repository = Substitute.For<IGalleryRepository>();
+
+    public GalleriesControllerTests()
+    {
+        _sut = new GalleriesController(_repository);
+    }
+
+    [Fact]
+    public async Task GetPublicGalleries_ShouldReturnGalleries_WhenGalleriesExist()
+    {
+        // Arrange
+        _repository.GetPublicGalleries()
+                   .Returns(new List<Gallery>());
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetPublicGalleries();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Gallery>>();
+    }
+
+    [Fact]
+    public async Task GetPublicGalleries_ShouldReturnNotFound_WhenNoGalleriesExist()
+    {
+        // Arrange
+        _repository.GetPublicGalleries()
+                   .ReturnsNull();
+
+        // Act
+        var result = (NotFoundResult)await _sut.GetPublicGalleries();
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetGalleryById_ShouldReturnGallery_WhenGalleryExists()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var gallery = new Gallery
+        {
+            Id = id,
+            Description = "Test",
+            Public = true,
+            Photos = new List<Photo>()
+        };
+
+        _repository.GetGalleryWithPhotos(id)
+                   .Returns(gallery);
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetGalleryById(id);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<Gallery>();
+    }
+
+    [Fact]
+    public async Task GetGalleryById_ShouldReturnNotFound_WhenGalleryDoesNotExist()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+
+        _repository.GetGalleryWithPhotos(id)
+                   .ReturnsNull();
+
+        // Act
+        var result = (NotFoundResult)await _sut.GetGalleryById(id);
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+}
---
+++ Tests/Server/Controllers/PhotosControllerTests.cs
@@ -0,0 +1,151 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using PhotoPortfolio.Server.Contracts;
+using PhotoPortfolio.Server.Controllers;
+using PhotoPortfolio.Shared.Entities;
+using PhotoPortfolio.Shared.Helpers;
+using PhotoPortfolio.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PhotoPortfolio.Tests.Server.Controllers;
+
+public class PhotosControllerTests : BaseApiController
+{
+    private readonly PhotosController _sut;
+    private readonly IPhotoRepository _repository = Substitute.For<IPhotoRepository>();
+
+    public PhotosControllerTests()
+    {
+        _sut = new PhotosController(_repository);
+    }
+
+    [Fact]
+    public async Task GetPhotos_WithParams_ReturnsPhotos()
+    {
+        // Arrange
+        var photoParams = new PhotoSpecificationParams
+        {
+            GalleryId = "1",
+            Title = "Test",
+            SortBy = "Custom",
+            SortOrder = "asc",
+        };
+
+        _repository.GetFilteredPhotosAsync(photoParams)
+                   .Returns(new List<Photo>());
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetPhotos(photoParams);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Photo>>();
+    }
+
+    [Fact]
+    public async Task GetPhotos_WithNullParams_ReturnsPhotos()
+    {
+        // Arrange
+        var photoParams = new PhotoSpecificationParams 
+        { 
+            GalleryId = null, 
+            Title = null, 
+            SortBy = null, 
+            SortOrder = null 
+        };
+
+        _repository.GetAllAsync()
+                   .Returns(new List<Photo>());
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetPhotos(photoParams);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<List<Photo>>();
+    }
+
+    [Fact]
+    public async Task GetPhotos_ShouldReturnNotFound_WhenNoPhotosExist()
+    {
+        // Arrange
+        var photoParams = new PhotoSpecificationParams
+        {
+            GalleryId = "1",
+            Title = "Test",
+            SortBy = "Custom",
+            SortOrder = "asc",
+        };
+
+        _repository.GetFilteredPhotosAsync(photoParams)
+                   .ReturnsNull();
+
+        // Act
+        var result = (NotFoundResult)await _sut.GetPhotos(photoParams);
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetPhotoById_WithValidId_ReturnsPhoto()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var photo = new Photo
+        {
+            Id = id,
+            Title = "Test",
+            Caption = "Caption",
+            FileName = "Test.jpg",
+            Uri = "https://localhost/images/Test.jpg",
+            GalleryId = "1",
+            GallerySortOrder = 1,
+            DateAdded = DateTime.UtcNow,
+            Metadata = new PhotoMetadata
+            {
+                Tags = new List<string> { "Test" },
+                Height = 100,
+                Width = 100
+            },
+            Products = new List<PhotoProduct>
+            {
+                new PhotoProduct
+                {
+                    Id = Guid.NewGuid().ToString(),
+                }
+            }
+        };
+
+        _repository.GetSingleAsync(Arg.Any<Expression<Func<Photo, bool>>>())
+                   .Returns(photo);
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetPhotoById(id);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeOfType<Photo>();
+    }
+
+    [Fact]
+    public async Task GetPhotoById_ShouldReturnNotFound_WhenPhotoDoesNotExist()
+    {
+          // Arrange
+        var id = Guid.NewGuid().ToString();
+
+        _repository.GetSingleAsync(Arg.Any<Expression<Func<Photo, bool>>>())
+                   .ReturnsNull();
+
+        // Act
+        var result = (NotFoundResult)await _sut.GetPhotoById(id);
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+}
