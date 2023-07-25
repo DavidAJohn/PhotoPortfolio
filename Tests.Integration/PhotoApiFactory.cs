77ae56c29a9f91813e62ec0a8d4b625e156e0dc4
David John <davidajohn@gmail.com>
2023-07-25 12:02:29 +0100

    Added a test container for mocking Stripe to the Api factory class

--- Tests.Integration/PhotoApiFactory.cs
+++ Tests.Integration/PhotoApiFactory.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +25,12 @@ public class PhotoApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 
     public string ConnectionString => _dbContainer.GetConnectionString();
 
+    private readonly IContainer _stripeContainer = 
+        new ContainerBuilder()
+            .WithImage("stripe/stripe-mock:latest")
+            .WithPortBinding(12111, true)
+            .Build();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureTestServices(services =>
@@ -35,18 +43,22 @@ public class PhotoApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
         builder.UseConfiguration(new ConfigurationBuilder()
                     .AddInMemoryCollection(new Dictionary<string, string?>
                     {
-                        { "SitePreferencesId", SitePreferencesId }
+                        { "SitePreferencesId", SitePreferencesId },
+                        { "Stripe:SecretKey", "sk_test_123" },
+                        { "Stripe:ApiBase", $"http://localhost:{_stripeContainer.GetMappedPublicPort(12111)}" }
                     })
                     .Build());
     }
 
     public async Task InitializeAsync()
     {
+        await _stripeContainer.StartAsync();
         await _dbContainer.StartAsync();
     }
 
     async Task IAsyncLifetime.DisposeAsync()
     {
+        await _stripeContainer.DisposeAsync();
         await _dbContainer.DisposeAsync();
     }
 }
