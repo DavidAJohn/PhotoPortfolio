f1d8b020322180cf4e7e44c7c2c16df37cd9af6a
David John <davidajohn@gmail.com>
2023-07-28 13:27:33 +0100

    Added integration tests for the Quote Service

---
+++ Tests.Integration/Server/Services/QuoteServiceTests.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using PhotoPortfolio.Server.Contracts;
+using PhotoPortfolio.Server.Data;
+using PhotoPortfolio.Server.Services;
+using PhotoPortfolio.Shared.Entities;
+using PhotoPortfolio.Shared.Models;
+
+namespace PhotoPortfolio.Tests.Integration.Server.Services;
+
+public class QuoteServiceTests : IClassFixture<PhotoApiFactory>
+{
+    private readonly PhotoApiFactory _apiFactory;
+    private readonly HttpClient _client;
+    private readonly MongoContext _mongoContext;
+    private readonly IConfiguration _configuration;
+    private readonly IConfigurationService _configService;
+    private readonly ILogger<QuoteService> _logger;
+    private readonly IPhotoRepository _photoRepository;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    private readonly IConfiguration _testConfiguration;
+
+    private readonly string _photoId = ObjectId.GenerateNewId().ToString();
+    private readonly string _productId = ObjectId.GenerateNewId().ToString();
+
+    public QuoteServiceTests(PhotoApiFactory apiFactory)
+    {
+        _apiFactory = apiFactory;
+        _client = _apiFactory.CreateClient();
+        _client.BaseAddress = new Uri("https://localhost/api/");
+        _client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+        _mongoContext = new MongoContext(_apiFactory.ConnectionString, PhotoApiFactory.TestDbName);
+        _configuration = _apiFactory.Services.GetRequiredService<IConfiguration>();
+        _configService = new ConfigurationService(_configuration);
+        _logger = new Logger<QuoteService>(new LoggerFactory());
+        _photoRepository = new PhotoRepository(_mongoContext);
+        _httpClientFactory = _apiFactory.Services.GetRequiredService<IHttpClientFactory>();
+
+        _testConfiguration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+    }
+
+    private OrderBasketDto CreateOrderBasketDto()
+    {
+        return new OrderBasketDto
+        {
+            BasketItems = new List<BasketItem>
+            {
+                new BasketItem
+                {
+                    Quantity = 1,
+                    Product = new ProductBasketItemDto
+                    {
+                        Id = _productId,
+                        PhotoId = _photoId,
+                        ImageUri = "https://localhost/images/1.jpg",
+                        ImageTitle = "Test",
+                        ProdigiSku = "ECO-CAN-16x24",
+                        Options = new List<ProductOptionSelected>
+                        {
+                            new ProductOptionSelected
+                            {
+                                OptionLabel = "wrap",
+                                OptionRef = "MirrorWrap",
+                            }
+                        }
+                    },
+                    Total = 100m,
+                }
+            },
+            ShippingMethod = "Standard",
+            ShippingCost = 5.25m
+        };
+    }
+
+    private Photo CreatePhoto()
+    {
+        var photo = new Photo
+        {
+            Id = _photoId,
+            Title = "Test",
+            Caption = "Caption",
+            FileName = "Test.jpg",
+            Uri = "https://localhost/images/Test.jpg",
+            GalleryId = "1",
+            GallerySortOrder = 1,
+            DateAdded = DateTime.UtcNow,
+            Metadata = new PhotoMetadata
+            {
+                Tags = new List<string> { "Test" },
+                Height = 100,
+                Width = 100
+            },
+            Products = new List<PhotoProduct>
+            {
+                new PhotoProduct
+                {
+                    Id = _productId,
+                    ProdigiSku = "ECO-CAN-16x24",
+                    MarkupPercentage = 100,
+                }
+            }
+        };
+
+        return photo;
+    }
+
+    [Fact]
+    public async Task GetBasketQuote_ReturnsOrderBasketDto_WhenIsAdminIsFalseQuoteIsReceived()
+    {
+        // Arrange
+        _configuration["Prodigi:ApiKey"] = "00000000-0000-0000-0000-000000000000";
+        _configuration["Prodigi:ApiUri"] = _testConfiguration["Prodigi:ApiUri"];
+        var quoteService = new QuoteService(_httpClientFactory, _configService, _logger, _photoRepository);
+
+        var photo = CreatePhoto();
+        var createdPhoto = await _photoRepository.AddAsync(photo);
+
+        var orderBasketDto = CreateOrderBasketDto();
+
+        // Act
+        var orderBasketReturned = await quoteService.GetBasketQuote(orderBasketDto, false);
+
+        // Assert
+        orderBasketReturned.Should().NotBeNull();
+        orderBasketReturned.Should().BeOfType<OrderBasketDto>();
+        orderBasketReturned.BasketItems.Should().NotBeEmpty();
+
+        // Clean up
+        await _photoRepository.DeleteAsync(createdPhoto.Id!);
+    }
+
+    [Fact]
+    public async Task GetBasketQuote_ReturnsOrderBasketDto_WhenIsAdminIsTrueQuoteIsReceived()
+    {
+        // Arrange
+        _configuration["Prodigi:ApiKey"] = "00000000-0000-0000-0000-000000000000";
+        _configuration["Prodigi:ApiUri"] = _testConfiguration["Prodigi:ApiUri"];
+        var quoteService = new QuoteService(_httpClientFactory, _configService, _logger, _photoRepository);
+
+        var photo = CreatePhoto();
+        var createdPhoto = await _photoRepository.AddAsync(photo);
+
+        var orderBasketDto = CreateOrderBasketDto();
+
+        // Act
+        var orderBasketReturned = await quoteService.GetBasketQuote(orderBasketDto, true);
+
+        // Assert
+        orderBasketReturned.Should().NotBeNull();
+        orderBasketReturned.Should().BeOfType<OrderBasketDto>();
+        orderBasketReturned.BasketItems.Should().NotBeEmpty();
+        // also check the markup percentage hasn't been applied
+        // orderBasketReturned.BasketItems.First().Product.MarkupPercentage.Should().Be(0);
+
+        // Clean up
+        await _photoRepository.DeleteAsync(createdPhoto.Id!);
+    }
+
+    [Fact]
+    public async Task GetBasketQuote_ReturnsOrderBasketDto_WhenQuoteIsReceivedAsCreatedWithIssues()
+    {
+        // Arrange
+        _configuration["Prodigi:ApiKey"] = "00000000-0000-0000-0000-000000000000";
+        _configuration["Prodigi:ApiUri"] = _testConfiguration["Prodigi:ApiUri"];
+        var quoteService = new QuoteService(_httpClientFactory, _configService, _logger, _photoRepository);
+
+        var photo = CreatePhoto();
+        var createdPhoto = await _photoRepository.AddAsync(photo);
+
+        var orderBasketDto = CreateOrderBasketDto();
+        orderBasketDto.ShippingMethod = "ReturnCreatedWithIssues"; // this will return a quote with issues
+
+        // Act
+        var orderBasketReturned = await quoteService.GetBasketQuote(orderBasketDto, false);
+
+        // Assert
+        orderBasketReturned.Should().NotBeNull();
+        orderBasketReturned.Should().BeOfType<OrderBasketDto>();
+        orderBasketReturned.BasketItems.Should().NotBeEmpty();
+
+        // Clean up
+        await _photoRepository.DeleteAsync(createdPhoto.Id!);
+    }
+}
