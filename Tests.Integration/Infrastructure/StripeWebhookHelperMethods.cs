a5994c6c1061e16d3ecb7c55e7b5a755d9be096e
David John <davidajohn@gmail.com>
2023-08-07 13:37:22 +0100

    Created integration tests for the Stripe Webhook endpoint in the Payments Controller

---
+++ Tests.Integration/Infrastructure/StripeWebhookHelperMethods.cs
@@ -0,0 +1,236 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PhotoPortfolio.Tests.Integration.Infrastructure;
+
+public class StripeWebhookHelperMethods
+{
+    // Parts of the code below have been adapted from the official open-source Stripe .NET library.
+    // Specifically, the method of generating the Stripe-Signature header value is taken from here:
+    // https://github.com/stripe/stripe-dotnet/blob/master/src/Stripe.net/Services/Events/EventUtility.cs#L193
+
+    private static readonly UTF8Encoding SafeUTF8 
+        = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+
+    public static Stripe.Event CreateCheckoutSessionCompleteEvent(string orderId)
+    {
+        var webhookEvent = new Stripe.Event
+        {
+            Id = "evt_" + Guid.NewGuid().ToString(),
+            Object = "event",
+            ApiVersion = "2022-11-15",
+            Created = DateTime.UtcNow,
+            Account = "acct_1",
+            Livemode = false,
+            Type = "checkout.session.completed",
+            PendingWebhooks = 0,
+            Request = new Stripe.EventRequest
+            {
+                Id = "req_" + Guid.NewGuid().ToString(),
+                IdempotencyKey = Guid.NewGuid().ToString()
+            },
+            Data = new Stripe.EventData
+            {
+                Object = new Stripe.Checkout.Session
+                {
+                    Id = $"cs_{Guid.NewGuid()}",
+                    Object = "checkout.session",
+                    AfterExpiration = null,
+                    AmountSubtotal = 10000,
+                    AmountTotal = 10525,
+                    AutomaticTax = new Stripe.Checkout.SessionAutomaticTax
+                    {
+                        Enabled = false,
+                        Status = null
+                    },
+                    BillingAddressCollection = null,
+                    CancelUrl = "https://localhost/checkout/cancel",
+                    ClientReferenceId = null,
+                    Currency = "gbp",
+                    PaymentStatus = "paid",
+                    CustomerDetails = new Stripe.Checkout.SessionCustomerDetails
+                    {
+                        Name = "Test User",
+                        Email = "test@test.com"
+                    },
+                    ShippingDetails = new Stripe.Checkout.SessionShippingDetails
+                    {
+                        Address = new Stripe.Address
+                        {
+                            Line1 = "Test Address",
+                            Line2 = "Test Address 2",
+                            City = "Test City",
+                            Country = "Test Country",
+                            PostalCode = "Test Postcode",
+                            State = "Test State"
+                        },
+                        Carrier = "Test Carrier",
+                        Name = "Test Name",
+                        Phone = "Test Phone",
+                        TrackingNumber = "Test Tracking Number"
+                    },
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "order_id", orderId },
+                        { "shipping_method", "Standard" }
+                    },
+                    PaymentIntentId = $"pi_{Guid.NewGuid()}"
+                }
+            }
+        };
+
+        return webhookEvent;
+    }
+
+    public static Stripe.Event CreatePaymentIntentCreatedEvent()
+    {
+        var webhookEvent = new Stripe.Event
+        {
+            Id = "evt_" + Guid.NewGuid().ToString(),
+            Object = "event",
+            ApiVersion = "2022-11-15",
+            Created = DateTime.UtcNow,
+            Account = "acct_1",
+            Livemode = false,
+            Type = "payment_intent.created",
+            PendingWebhooks = 0,
+            Request = new Stripe.EventRequest
+            {
+                Id = "req_" + Guid.NewGuid().ToString(),
+                IdempotencyKey = Guid.NewGuid().ToString()
+            },
+            Data = new Stripe.EventData
+            {
+                Object = new Stripe.PaymentIntent
+                {
+                    Id = $"pi_{Guid.NewGuid()}",
+                    Object = "payment_intent"
+                }
+            }
+        };
+
+        return webhookEvent;
+    }
+
+    public static Stripe.Event CreatePaymentIntentSucceededEvent()
+    {
+        var webhookEvent = new Stripe.Event
+        {
+            Id = "evt_" + Guid.NewGuid().ToString(),
+            Object = "event",
+            ApiVersion = "2022-11-15",
+            Created = DateTime.UtcNow,
+            Account = "acct_1",
+            Livemode = false,
+            Type = "payment_intent.succeeded",
+            PendingWebhooks = 0,
+            Request = new Stripe.EventRequest
+            {
+                Id = "req_" + Guid.NewGuid().ToString(),
+                IdempotencyKey = Guid.NewGuid().ToString()
+            },
+            Data = new Stripe.EventData
+            {
+                Object = new Stripe.PaymentIntent
+                {
+                    Id = $"pi_{Guid.NewGuid()}",
+                    Object = "payment_intent"
+                }
+            }
+        };
+
+        return webhookEvent;
+    }
+
+    public static Stripe.Event CreateChargeSucceededEvent()
+    {
+        var webhookEvent = new Stripe.Event
+        {
+            Id = "evt_" + Guid.NewGuid().ToString(),
+            Object = "event",
+            ApiVersion = "2022-11-15",
+            Created = DateTime.UtcNow,
+            Account = "acct_1",
+            Livemode = false,
+            Type = "charge.succeeded",
+            PendingWebhooks = 0,
+            Request = new Stripe.EventRequest
+            {
+                Id = "req_" + Guid.NewGuid().ToString(),
+                IdempotencyKey = Guid.NewGuid().ToString()
+            },
+            Data = new Stripe.EventData
+            {
+                Object = new Stripe.Charge
+                {
+                    Id = $"ch_{Guid.NewGuid()}",
+                    Object = "charge",
+                    Status = "succeeded"
+                }
+            }
+        };
+
+        return webhookEvent;
+    }
+
+    public static Stripe.Event CreateChargeFailedEvent()
+    {
+        var webhookEvent = new Stripe.Event
+        {
+            Id = "evt_" + Guid.NewGuid().ToString(),
+            Object = "event",
+            ApiVersion = "2022-11-15",
+            Created = DateTime.UtcNow,
+            Account = "acct_1",
+            Livemode = false,
+            Type = "charge.failed",
+            PendingWebhooks = 0,
+            Request = new Stripe.EventRequest
+            {
+                Id = "req_" + Guid.NewGuid().ToString(),
+                IdempotencyKey = Guid.NewGuid().ToString()
+            },
+            Data = new Stripe.EventData
+            {
+                Object = new Stripe.Charge
+                {
+                    Id = $"ch_{Guid.NewGuid()}",
+                    Object = "charge",
+                    Status = "failed"
+                }
+            }
+        };
+
+        return webhookEvent;
+    }
+
+    public static HttpContent CreateWebhookContent(Stripe.Event webhookEvent, string stripeWebhookSecret)
+    {
+        // For details of how the Stripe webhook signature is created, see https://stripe.com/docs/webhooks#verify-manually
+        // This is used here to create a valid webhook event for testing purposes
+
+        var datetimeOffset = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        var jsonPayloadIndented = JsonConvert.SerializeObject(webhookEvent, Formatting.Indented); // don't switch to using System.Text.Json here, it doesn't work with the Stripe.net library
+        var jsonPayload = jsonPayloadIndented.Replace("\r", string.Empty); // remove additional '\r' carriage returns that Newtonsoft.Json adds
+        var signedPayload = $"{datetimeOffset}.{jsonPayload}";
+
+        var secretBytes = SafeUTF8.GetBytes(stripeWebhookSecret);
+        var payloadBytes = SafeUTF8.GetBytes(signedPayload);
+
+        var signature = string.Empty;
+
+        using (var hmac = new HMACSHA256(secretBytes))
+        {
+            var hash = hmac.ComputeHash(payloadBytes);
+            signature = BitConverter.ToString(hash).Replace("-", string.Empty).ToLowerInvariant();
+        }
+
+        HttpContent webhookContent = new StringContent(jsonPayload);
+        webhookContent.Headers.Add("Stripe-Signature", $"t={datetimeOffset},v1={signature}");
+        webhookContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        return webhookContent;
+    }
+}
---
+++ Tests.Integration/Server/Controllers/PaymentsController/StripeWebhookTests.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using PhotoPortfolio.Server.Contracts;
+using PhotoPortfolio.Server.Data;
+using PhotoPortfolio.Server.Services;
+using PhotoPortfolio.Shared.Models;
+using PhotoPortfolio.Tests.Integration.Infrastructure;
+using System.Net;
+
+namespace PhotoPortfolio.Tests.Integration.Server.Controllers.PaymentsController;
+
+public class StripeWebhookTests : IClassFixture<PhotoApiFactory>
+{
+    private readonly PhotoApiFactory _apiFactory;
+    private readonly HttpClient _client;
+    private readonly MongoContext _mongoContext;
+    private readonly IConfiguration _configuration;
+    private readonly IConfigurationService _configService;
+    private readonly IOrderService _orderService;
+    private readonly IOrderRepository _orderRepository;
+    private readonly ILogger<OrderService> _logger;
+    private readonly IPreferencesRepository _preferencesRepository;
+
+    public StripeWebhookTests(PhotoApiFactory apiFactory)
+    {
+        _apiFactory = apiFactory;
+        _client = _apiFactory.CreateClient();
+        _client.BaseAddress = new Uri("https://localhost/api/");
+
+        _configuration = _apiFactory.Services.GetRequiredService<IConfiguration>();
+        _configService = new ConfigurationService(_configuration);
+        _mongoContext = new MongoContext(_apiFactory.ConnectionString, PhotoApiFactory.TestDbName);
+        _preferencesRepository = new PreferencesRepository(_mongoContext);
+        _orderRepository = new OrderRepository(_mongoContext);
+        _orderService = new OrderService(_orderRepository, _preferencesRepository, _configService, _logger);
+    }
+
+    private static OrderBasketDto CreateOrderBasketDto()
+    {
+        return new OrderBasketDto
+        {
+            BasketItems = new List<BasketItem>
+            {
+                new BasketItem
+                {
+                    Quantity = 1,
+                    Product = new ProductBasketItemDto
+                    {
+                        Id = ObjectId.GenerateNewId().ToString(),
+                        PhotoId = ObjectId.GenerateNewId().ToString(),
+                        ImageUri = "https://localhost/images/1.jpg",
+                        ImageTitle = "Test",
+                    },
+                    Total = 100m,
+                }
+            },
+            ShippingMethod = "Standard",
+            ShippingCost = 5.25m
+        };
+    }
+
+    [Fact]
+    public async Task StripeWebhook_ReturnsOkAndUpdatesOrder_WhenCheckoutSessionCompletes()
+    {
+        // Arrange
+        var orderBasketDto = CreateOrderBasketDto();
+        var orderId = await _orderService.CreatOrder(orderBasketDto);
+
+        var config = _configService.GetConfiguration();
+        var stripeWebhookSecret = config["Stripe:WhSecret"];
+
+        var webhookEvent = StripeWebhookHelperMethods.CreateCheckoutSessionCompleteEvent(orderId);
+        var webhookContent = StripeWebhookHelperMethods.CreateWebhookContent(webhookEvent, stripeWebhookSecret!);
+
+        // Act
+        var response = await _client.PostAsync("payments/webhook", webhookContent);
+        var updatedOrder = await _orderService.GetOrderDetailsFromId(orderId);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        updatedOrder.Id.Should().Be(orderId);
+        updatedOrder.Status.Should().Be(OrderStatus.AwaitingApproval.ToString());
+
+        var sessionDataObj = (Stripe.Checkout.Session)webhookEvent.Data.Object;
+        updatedOrder.Id.Should().Be(sessionDataObj.Metadata["order_id"]);
+        updatedOrder.Name.Should().Be(sessionDataObj.CustomerDetails.Name);
+        updatedOrder.EmailAddress.Should().Be(sessionDataObj.CustomerDetails.Email);
+        updatedOrder.Address.Line1.Should().Be(sessionDataObj.ShippingDetails.Address.Line1);
+        updatedOrder.Address.Line2.Should().Be(sessionDataObj.ShippingDetails.Address.Line2);
+        updatedOrder.Address.TownOrCity.Should().Be(sessionDataObj.ShippingDetails.Address.City);
+        updatedOrder.Address.PostalOrZipCode.Should().Be(sessionDataObj.ShippingDetails.Address.PostalCode);
+        updatedOrder.Address.CountryCode.Should().Be(sessionDataObj.ShippingDetails.Address.Country);
+        updatedOrder.Address.StateOrCounty.Should().Be(sessionDataObj.ShippingDetails.Address.State);
+
+        // Clean up
+        await _orderRepository.DeleteAsync(orderId);
+    }
+
+    [Fact]
+    public async Task StripeWebhook_ReturnsOk_WhenPaymentIntentCreatedEventReceived()
+    {
+        // Arrange
+        var config = _configService.GetConfiguration();
+        var stripeWebhookSecret = config["Stripe:WhSecret"];
+
+        var webhookEvent = StripeWebhookHelperMethods.CreatePaymentIntentCreatedEvent();
+        var webhookContent = StripeWebhookHelperMethods.CreateWebhookContent(webhookEvent, stripeWebhookSecret!);
+
+        // Act
+        var response = await _client.PostAsync("payments/webhook", webhookContent);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task StripeWebhook_ReturnsOk_WhenPaymentIntentSucceededEventReceived()
+    {
+        // Arrange
+        var config = _configService.GetConfiguration();
+        var stripeWebhookSecret = config["Stripe:WhSecret"];
+
+        var webhookEvent = StripeWebhookHelperMethods.CreatePaymentIntentSucceededEvent();
+        var webhookContent = StripeWebhookHelperMethods.CreateWebhookContent(webhookEvent, stripeWebhookSecret!);
+
+        // Act
+        var response = await _client.PostAsync("payments/webhook", webhookContent);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task StripeWebhook_ReturnsOk_WhenChargeSucceededEventReceived()
+    {
+        // Arrange
+        var config = _configService.GetConfiguration();
+        var stripeWebhookSecret = config["Stripe:WhSecret"];
+
+        var webhookEvent = StripeWebhookHelperMethods.CreateChargeSucceededEvent();
+        var webhookContent = StripeWebhookHelperMethods.CreateWebhookContent(webhookEvent, stripeWebhookSecret!);
+
+        // Act
+        var response = await _client.PostAsync("payments/webhook", webhookContent);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
