8864c21d619ae13b1a137a0e22c5fa1b7a4e0d91
David John <davidajohn@gmail.com>
2023-08-14 15:38:12 +0100

    Added more e2e tests for the GalleryIndex page

--- Tests.e2e/Pages/GalleryIndexTests.cs
+++ Tests.e2e/Pages/GalleryIndexTests.cs
@@ -15,4 +15,37 @@ public class GalleryIndexTests : LocalBlazorTestBase
         await Expect(Page).ToHaveTitleAsync(new Regex("PhotoPortfolio"));
         await Expect(Page).ToHaveURLAsync(new Regex("/gallery"));
     }
+
+    [Test]
+    public async Task GalleryIndexPage_ShouldShowListOfGalleries()
+    {
+        //await Page.PauseAsync();
+
+        await Page.GotoAsync(RootUri.AbsoluteUri + "gallery");
+
+        var galleries = Page.Locator("#gallery-container");
+        await Expect(galleries).ToHaveCountAsync(3); // public galleries from GalleryList
+    }
+
+    [Test]
+    public async Task GalleryIndexPage_ShouldGoToSpecificGalleryWhenSeeMoreLinkClicked()
+    {
+        //await Page.PauseAsync();
+
+        await Page.GotoAsync(RootUri.AbsoluteUri);
+
+        var galleryName = "Landscapes";
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Gallery", Exact = true }).ClickAsync();
+
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = galleryName })).ToBeVisibleAsync();
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "See More ➜" }).First.ClickAsync();
+
+        await Expect(Page).ToHaveURLAsync(new Regex("/galleries/"));
+
+        await Expect(Page.Locator("div").Filter(new() { HasText = $"Gallery {galleryName}" }).Nth(3)).ToBeVisibleAsync();
+
+        await Expect(Page.Locator("#text-block-content")).ToHaveTextAsync(galleryName);
+    }
 }
