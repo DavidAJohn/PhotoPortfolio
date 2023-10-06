211ccf5500b219f99a490166c2528d9211a0a5ed
David John <davidajohn@gmail.com>
2023-10-06 14:56:53 +0100

    Created an OrderApproved class and a custom mapper to use with messaging

---
+++ Server/Mapping/OrderDetailsToOrderApprovedMapper.cs
@@ -0,0 +1,24 @@
+﻿using PhotoPortfolio.Server.Messaging;
+using PhotoPortfolio.Shared.Models;
+
+namespace PhotoPortfolio.Server.Mapping;
+
+public static class OrderDetailsToOrderApprovedMapper
+{
+    public static OrderApproved ToOrderApprovedMessage(this OrderDetailsDto orderDetailsDto)
+    {
+        return new OrderApproved
+        {
+            Id = orderDetailsDto.Id,
+            Name = orderDetailsDto.Name,
+            EmailAddress = orderDetailsDto.EmailAddress,
+            OrderDate = orderDetailsDto.OrderDate,
+            Items = orderDetailsDto.Items,
+            ShippingCost = orderDetailsDto.ShippingCost,
+            TotalCost = orderDetailsDto.TotalCost,
+            Address = orderDetailsDto.Address,
+            ShippingMethod = orderDetailsDto.ShippingMethod,
+            StripePaymentIntentId = orderDetailsDto.StripePaymentIntentId
+        };
+    }
+}
---
+++ Server/Messaging/OrderApproved.cs
@@ -0,0 +1,17 @@
+﻿using PhotoPortfolio.Shared.Entities;
+using PhotoPortfolio.Shared.Models.Prodigi.Orders;
+
+namespace PhotoPortfolio.Server.Messaging;
+
+public class OrderApproved : BaseEntity
+{
+    public string Name { get; set; }
+    public string EmailAddress { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<BasketItem> Items { get; set; }
+    public decimal ShippingCost { get; set; }
+    public decimal TotalCost { get; set; }
+    public Address Address { get; set; }
+    public string ShippingMethod { get; set; }
+    public string StripePaymentIntentId { get; set; }
+}
