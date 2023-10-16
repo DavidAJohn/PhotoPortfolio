91b1135f2f1ec024df0e9062e1d268e582911e0e
David John <davidajohn@gmail.com>
2023-10-16 14:33:57 +0100

    Created a custom mapper class to convert order details into Prodigi orders

---
+++ Server/Mapping/OrderDetailsToProdigiOrderMapper.cs
@@ -0,0 +1,58 @@
+﻿using PhotoPortfolio.Shared.Models;
+using PhotoPortfolio.Shared.Models.Prodigi.Orders;
+
+namespace PhotoPortfolio.Server.Mapping;
+
+public static class OrderDetailsToProdigiOrderMapper
+{
+    public static Order ToProdigiOrder(this OrderDetailsDto orderDetailsDto)
+    {
+        string merchantReference = $"PhotoPortfolio_{orderDetailsDto.Id![16..]}";
+        string callbackUrl = $"https://localhost:7200/callbacks";
+
+        return new Order
+        {
+            CallbackUrl = callbackUrl,
+            MerchantReference = merchantReference,
+            ShippingMethod = orderDetailsDto.ShippingMethod,
+            IdempotencyKey = orderDetailsDto.Id,
+            Recipient = new Recipient
+            {
+                Name = orderDetailsDto.Name,
+                Email = orderDetailsDto.EmailAddress,
+                Address = orderDetailsDto.Address
+            },
+            Items = orderDetailsDto.Items.Select(item => new Item
+            {
+                MerchantReference = item.Product.PhotoId,
+                Sku = item.Product.ProdigiSku,
+                Copies = item.Quantity,
+                Sizing = "fillPrintArea",
+                Attributes = item.Product.Options.ToAttributes(),
+                Assets = new List<Dictionary<string, string>>
+                {
+                    new Dictionary<string, string>
+                    {
+                        { "printArea", "default" },
+                        { "url", item.Product.ImageUri }
+                    }
+                }
+            }).ToList()
+        };
+    }
+
+    private static Dictionary<string, string> ToAttributes(this IEnumerable<ProductOptionSelected>? productOptions)
+    {
+        Dictionary<string, string>? attributes = new() { };
+
+        if (productOptions is not null)
+        {
+            foreach (var attribute in productOptions)
+            {
+                attributes.Add(attribute.OptionLabel, attribute.OptionRef);
+            }
+        }
+
+        return attributes;
+    }
+}
