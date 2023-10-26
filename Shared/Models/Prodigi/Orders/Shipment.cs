6554eec4c5d5f863f25f5d2c2aa15dfd321605ef
David John <davidajohn@gmail.com>
2023-10-26 18:19:13 +0100

    Fixed a problem with the Prodigi Order model

--- Shared/Models/Prodigi/Orders/Shipment.cs
+++ Shared/Models/Prodigi/Orders/Shipment.cs
@@ -5,9 +5,9 @@ namespace PhotoPortfolio.Shared.Models.Prodigi.Orders;
 public class Shipment
 {
     public string Id { get; set; }
-    public string Carrier { get; set; } = string.Empty;
-    public string Tracking { get; set; } = string.Empty;
     public string DispatchDate { get; set; }
-    public List<ShipmentItem> ShipmentItems { get; set; }
+    public Carrier Carrier { get; set; }
+    public Dictionary<string, string>? Tracking { get; set; }
+    public List<ShipmentItem> Items { get; set; }
     public FulfillmentLocation FulfillmentLocation { get; set; }
 }
